8f5a1b65988e7ba028061e74100134a603db0e4f
BastienGavioli <bastien.gavioli@etu.umontpellier.fr>
2022-03-13 23:23:52 +0100

    Update footer, buttons and header

--- js/app.js
+++ js/app.js
@@ -5,19 +5,11 @@ toggle.addEventListener('click', function () {
     document.body.classList.toggle('open');
     compteurNav++;
     // if scroll happens, set it to the previous value
+    // else, unable to scroll
     if (compteurNav % 2 === 1){
         // To get the scroll position of current webpage
-        const topScroll = window.scrollY || document.documentElement.scrollTop;
         const leftScroll = window.scrollX || document.documentElement.scrollLeft;
-        window.onscroll = function() { window.scrollTo(leftScroll, topScroll); };
-    } else {
-        window.onscroll = function() {};
-    }
-
-});
-
-window.addEventListener('scroll', function (){
-    if (window.scrollY > 50)
-        document.getElementsByTagName("header")[0].style.boxShadow = "0 1px 5px black";
-    else document.getElementsByTagName("header")[0].style.boxShadow = "";
+        const topScroll = window.scrollY || document.documentElement.scrollTop;
+        window.onscroll = () => window.scrollTo(leftScroll, topScroll);
+    } else window.onscroll = () => {};
 });
